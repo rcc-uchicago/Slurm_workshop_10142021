7f9c229d7e836620a85cfd7bb1c783f19b926578
Kyle Ding <dingkaihua@gmail.com>
2021-10-13 22:06:01 -0500

    adding all tutorials

---
+++ Tutorial4_MPI_and_GPU/mpijob.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH -t 00:30:00
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=4
+#SBATCH --partition=gpu2
+#SBATCH --gres=gpu:2
+##SBATCH --nodelist=gpu1203,gpu1204
+#SBATCH --job-name=MyJob
+#SBATCH --output=MyJob-%j.out
+#SBATCH --error=MyJob-%j.err
+mpirun ./HostMap
